987549b126d6813ff0113c9c493a46bc2495ef8f
Shubham <shubhamguptakrj2008@gmail.com>
2022-12-05 00:37:28 +0530

    nav test

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,38 +1,36 @@
 import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Navbar from "../components/Navbar";
 
-const Home = ({ navigation }) => {
+const Navbar = () => {
+  const navigation = useNavigation();
   return (
     <View style={styles.main}>
-      <Text>Home</Text>
       <Button
+        style={styles.btn}
         title="Go to Student"
         onPress={() => navigation.navigate("Student")}
       />
-      <View style={styles.nav}>
-        <Navbar />
-      </View>
     </View>
   );
 };
 
-export default Home;
+export default Navbar;
 
 const styles = StyleSheet.create({
   main: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
+    flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCFF",
   },
-  nav: {
-    height: 100,
-    width: "100%",
-    borderColor: "black",
-    borderTopWidth: 1,
+  btn: {
+    backgroundColor: "#3b5998",
+    padding: 50,
+    margin: 10,
+    borderRadius: 10,
+    width: 10,
+    height: 10,
   },
 });
